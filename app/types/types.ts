4f7400a3718d33c352acd11f276bbd79bed8261c
MattMW04 <6wilcm63@solent.ac.uk>
2025-06-03 21:22:29 +0100

    Removed all references to NextAuth

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,16 +1,2 @@
-// User Type
-export type UserRole = "admin" | "staff" | "student";
 
-// User
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  role: UserRole;
-}
-
-// Separate type for authentication
-export interface AuthUser extends User {
-  password: string;
-}
 
--- middleware.ts
+++ middleware.ts
@@ -1,58 +1,12 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+//import { NextResponse } from "next/server";
+//import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
-  const { pathname } = req.nextUrl;
-
-  // Allow NextAuth routes to bypass auth checks
-  if (pathname.startsWith("/api/auth")) {
-    return NextResponse.next();
-  }
-  // Redirect if not authenticated
-  if (!token) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  // Admin routes protection
-  if (pathname.startsWith("/admin") && token.account_type !== "admin") {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  // Staff routes protection
-  if (
-    pathname.startsWith("/dashboard/staff") &&
-    token.account_type !== "staff"
-  ) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  // Student routes protection
-  if (
-    pathname.startsWith("/dashboard/student") &&
-    token.account_type !== "student"
-  ) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  // API routes protection for non logged in users - keeping access to login routes
-  if (pathname.startsWith("/api") && !token) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-
-
-
-  return NextResponse.next();
-}
+//export async function middleware(req: NextRequest) {
+//return NextResponse.next();
+//}
 
 // Apply middleware to these routes
-export const config = {
-  matcher: [
-    "/admin/:path*",
-    "/dashboard/staff/:path*",
-    "/dashboard/student/:path*",
-    "/api/:path*",
-  ],
-};
+//export const config = {
+//matcher: [
+//],
+//};
